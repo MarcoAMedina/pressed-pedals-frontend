57ad030162c9faf6a74439a058dfcf3801e50920
MarcoAMedina <medinaam94@gmail.com>
2022-07-10 00:08:12 -0500

    Adds logout router link

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,8 @@
     <router-link to="/signup">Signup</router-link>
     |
     <router-link to="/login">Login</router-link>
+    |
+    <router-link to="/logout">Logout</router-link>
   </nav>
   <router-view />
 </template>
